8c6f14f416eca4a39f7c3acdc9a77e1033f9077d
Sunghee2 <630sunghee@naver.com>
2018-07-17 21:21:58 +0900

    add StudentTeacherViewModel

--- MyApp/Controllers/HomeController.cs
+++ MyApp/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MyApp.Models;
+using MyApp.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,12 +15,26 @@ namespace MyApp.Controllers
         // GET: /<controller>/
         public IActionResult Student()
         {
-            return View();
+            List<Teacher> teachers = new List<Teacher>()
+            {
+                new Teacher(){ Name = "세종대왕", Class = "한글" },
+                new Teacher(){ Name = "이순신", Class = "해상전략" },
+                new Teacher(){ Name = "제갈량", Class = "지략" },
+                new Teacher(){ Name = "을지문덕", Class = "지상전략" }
+            };
+
+            var viewModel = new StudentTeacherViewModel()
+            {
+                Student = new Student(),
+                Teachers = teachers
+            };
+
+            return View(viewModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Student(Student model)
+        public IActionResult Student(StudentTeacherViewModel model)
         {
             // model에 있는 유효성 검사 확인
             if(ModelState.IsValid)
